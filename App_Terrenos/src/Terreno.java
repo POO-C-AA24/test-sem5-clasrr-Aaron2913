b8461181ff05387b927599d1438a4048b599cb6b
Aaron2913 <149200543+Aaron2913@users.noreply.github.com>
2024-05-09 16:04:07 -0500

    Add files via upload

--- App_Terrenos/src/Terreno.java
+++ App_Terrenos/src/Terreno.java
@@ -1,12 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author utpl
- */
-public class Terreno {
-    
+public class Terreno{
+    public double alto, ancho, vmc, area, costoFinal;
+    public Terreno(double alto, double ancho, double vmc) {
+        this.alto = alto;
+        this.ancho = ancho;
+        this.vmc = vmc;
+    }
+    public void calcularArea(){
+        this.area = this.alto*this.ancho;}
+    public void calcularCostoFinal(){
+        costoFinal = area*vmc;
+        if (area > 300)
+            costoFinal *= 0.8; //costoFinal =  (area*vmc) - ( area*vmc)*0.2;
+        else if (area >= 200)
+                costoFinal *= 0.9; //costoFinal =  (area*vmc) - (area*vmc)*0.1;
+    }
+    @Override
+    public String toString() {
+        return "Terreno{" + "alto=" + alto + ", ancho=" + ancho + ", vmc=" + vmc + ", area=" + area + ", costoFinal=" + costoFinal + '}';
+    }
 }
--- App_Terrenos/src/TestTerreno.java
+++ App_Terrenos/src/TestTerreno.java
@@ -1,12 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author utpl
- */
 public class TestTerreno {
-    
+    public static void main(String[] args) {
+        Terreno t1 = new Terreno(40, 10, 2);
+        Terreno t2 = new Terreno(21, 12, 2);
+        Terreno t3 = new Terreno(10, 10, 2);
+        t1.calcularArea();
+        t1.calcularCostoFinal();
+        System.out.println(t1);
+        t2.calcularArea();
+        t2.calcularCostoFinal();
+        System.out.println(t2);
+        t3.calcularArea();
+        t3.calcularCostoFinal();
+        System.out.println(t3);
+    }
 }
